936c951f3d1914b3ecf2127d56a1129719b7ddd1
Jonas Pammer <opensource@jonaspammer.at>
2023-06-07 15:30:51 +0200

    feat: open-source windows setup scripts (#12)
    Attributions given where due

---
+++ provisioner-windows/pc-setup.ps1
@@ -0,0 +1,230 @@
+<#
+.SYNOPSIS
+   Jonas Pammer's PC Setup Script.
+   This script aims to be fully automated and not ask anything.
+
+.EXAMPLE
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    .\pc-setup.ps1
+
+.NOTES
+  Author:         Jonas Pammer
+  Creation Date:  2022-03-02
+
+.PARAMETER Elevated
+    This parameter is for internal use to check whether an UAC prompt has already been attempted.
+#>
+Param(
+  [switch]$Elevated
+)
+
+### Constants
+$UpdateAppsIfInstalled = $true
+
+
+### Init
+. ".\utils.ps1"
+Start-Transcript -Path "${LogsPath}\$($MyInvocation.MyCommand.Name)--$(Get-Date -Format "yyyy-MM-dd--HH_mm_ss").txt"
+Elevate($MyInvocation.MyCommand.Definition)
+$Config = InitializeYAMLConfig
+
+### Functions
+function InstallAndUpdateApplications() {
+  Show-Output ">> Install and Update Applications"
+  Install-Chocolatey
+  Install-Winget
+
+  Show-Output "Reading .\applications.yml"
+  $_content = Get-Content -Raw ".\applications.yml"
+  $applicationsYAML = ConvertFrom-YAML -Ordered $_content
+
+  Show-Output "Updating all winget sources"
+  winget source update
+  Write-Host ""
+
+  ForEach ($app in $applicationsYAML.applications) {
+    #$app
+    Show-Output "-> Looping: " $app.display_name " (" $app.description_short ")"
+    if ($app.status -eq "not-used") {
+      Show-Output "--> Skipping (status: not-used)"
+    }
+    elseif ($app.provider -eq "winget") {
+      $listApp = winget list  --accept-source-agreements --exact -q $app.winget_id
+      if (![String]::Join("", $listApp).Contains($app.winget_id) -And !$app.uninstall) {
+        # if ($app.interactive) {
+        #   Show-Output "[WINGET] --> Installing " $app.winget_id " in interactive mode..."
+        #   winget install --accept-source-agreements --accept-package-agreements --exact --id $app.winget_id --interactive --scope $app.winget_scope
+        # }
+        # else
+        if ($null -eq $app.winget_scope) {
+          Show-Output "--> Installing " $app.winget_id " in silent, non-interactive mode [ambigious scope]..."
+          winget install --accept-source-agreements --accept-package-agreements --exact --id $app.winget_id --silent
+        }
+        else {
+          Show-Output "--> Installing " $app.winget_id " in silent, non-interactive mode..."
+          winget install --accept-source-agreements --accept-package-agreements --exact --id $app.winget_id --silent --scope $app.winget_scope
+        }
+      }
+      else {
+        if ($app.uninstall) {
+          Show-Output "--> Uninstalling " $app.winget_id "..."
+          winget uninstall $app.winget_id
+        }
+        elseif ($UpdateAppsIfInstalled) {
+          Show-Output "--> Updating " $app.winget_id "..."
+          winget upgrade $app.winget_id
+        }
+      }
+    }
+    elseif ($app.provider -eq "chocolatey") {
+      Show-Output "Installing " $app.chocolatey_name "..."
+      #choco feature enable -n=allowGlobalConfirmation#
+      choco install -y $app.chocolatey_name
+      #choco feature disable -n=allowGlobalConfirmation#
+    }
+    else {
+      Show-Output "--> Application does not have an installation provider!"
+      if ([bool]($app.PSobject.Properties.name -match "link") -And ($app.link)) {
+        Show-Output "--> Install/Download yourself at " $app.link
+      }
+    }
+    Write-Host ""
+  }
+  Write-Host ""
+}
+
+function SetupPowershellProfile() {
+  Show-Output ">> Setup Powershell Profile"
+  ## PowerShell environment for Git (e.g. adds tab completion)
+  Show-Output "Adding PoshGit to PowerShell Profile"
+  Set-ExecutionPolicy RemoteSigned -Scope Process
+  PowerShellGet\Install-Module posh-git -Scope AllUsers -Force
+  PowerShellGet\Update-Module posh-git
+  # This will add a line containing Import-Module posh-git to the file $profile.CurrentUserAllHosts:
+  Add-PoshGitToProfile -AllHosts
+
+  ## PowerShell helpers for SSH (e.g. Start-SshAgent -Quiet)
+  Show-Output "Adding PoshSshell to PowerShell Profile"
+  PowerShellGet\Install-Module posh-sshell -Scope AllUsers -Force
+  PowerShellGet\Update-Module posh-sshell
+  # This will add a line containing Import-Module posh-sshell to the file $profile.CurrentUserAllHosts:
+  Add-PoshSshellToProfile -AllHosts
+
+  Show-Output "Adding StartSshAgent Command to PowerShell Profile"
+  Get-Service -Name ssh-agent | Set-Service -StartupType Manual
+  $SEL = Select-String -Path $profile.CurrentUserAllHosts -Pattern "Start-SshAgent -Quiet"
+  if ($null -eq $SEL) {
+    Add-Content -Path $profile.CurrentUserAllHosts -Value "`r`nStart-SshAgent -Quiet"
+  }
+  Write-Host ""
+}
+
+function ConfigureGit() {
+  Show-Output ">> Configure Git"
+  Show-Output "Changing max-cache-ttl in gpg-agent.conf..."
+  # (assigning to a variable to make output silent)
+  Set-Content -Path "${env:APPDATA}\gnupg\gpg-agent.conf" -Value "default-cache-ttl 86400$([System.Environment]::NewLine)max-cache-ttl 86400"
+  gpgconf.exe --reload gpg-agent
+  # Verify with: gpgconf.exe --list-options gpg-agent
+
+  Show-Output "Executing commands to alter git config..."
+  $gpg_program = $(Get-Command gpg | Select-Object source)
+  $gpg_program_source = $gpg_program.Source # i don't have much ps1 knowledge
+  git config --global gpg.program "$gpg_program_source"
+
+  git config --global pull.rebase true
+
+  git config --global user.name "Jonas Pammer"
+  git config --global user.email "opensource@jonaspammer.at"
+
+  git config --global diff.colorMoved "zebra"
+  # Use better, descriptive initials (c, i, w) instead of a/b.
+  git config --global diff.mnemonicPrefix "true"
+  # Show renames/moves as such
+  git config --global diff.renames "true"
+
+  # Auto-fetch submodule changes (sadly, won't auto-update)
+  git config --global fetch.recurseSubmodules "on-demand"
+
+  git config --global grep.break "true"
+  git config --global grep.heading "true"
+  git config --global grep.lineNumber "true"
+
+  # Use abbrev SHAs whenever possible/relevant instead of full 40 chars
+  git config --global log.abbrevCommit "true"
+  # Automatically --follow when given a single path
+  git config --global log.follow "true"
+  # Disable decorate for reflog
+  # (because there is no dedicated `reflog` section available)
+  git config --global log.decorate "false"
+
+  # Clean up backup files created by merge tools on tool exit
+  git config --global mergetool.keepBackup "false"
+  # Clean up temp files created by merge tools on tool exit
+  git config --global mergetool.keepTemporaries "false"
+  # Put the temp files in a dedicated dir anyway
+  git config --global mergetool.writeToTemp "true"
+  # Auto-accept file prompts when launching merge tools
+  git config --global mergetool.prompt "false"
+
+  git config --global diff.tool vscode
+  git config --global "difftool.vscode.cmd" "code --wait --diff $LOCAL $REMOTE"
+
+  git config --global merge.tool "vscode"
+  git config --global "mergetool.vscode.cmd" "code --wait --merge $REMOTE $LOCAL $BASE $MERGED"
+
+  # Recursively traverse untracked directories to display all contents
+  git config --global status.showUntrackedFiles "all"
+
+  git config --global --add --bool push.autoSetupRemote true
+  Write-Host ""
+}
+
+function AddGodMode() {
+  Show-Output ">> Add Windows God Mode Icon to desktop"
+  $GodModeSplat = @{
+    Path     = "$HOME\Desktop"
+    Name     = "GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
+    ItemType = 'Directory'
+  }
+  $null = new-item @GodModeSplat
+  Write-Host ""
+}
+
+function Install-WSL() {
+  param (
+    [Parameter(Mandatory = $true)] [string] $distribution
+  )
+  Show-Output ">> Install WSL"
+  Show-Output "Enabling WSL features"
+  dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+  dism /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+  # no longer needed:
+  #.\wsl_update_x64.msi /quiet
+  # no longer exists, ""replaced"" by --enable-wsl1 as wsl2 is now default
+  #wsl --set-default-version 2
+  Show-Output "Install WSL Distribution '$distribution'"
+  wsl --install --distribution "$distribution"
+  Write-Host ""
+}
+
+
+### Main
+UpdateWindows
+AddGodMode
+Install-1PasswordCLI
+InstallAndUpdateApplications
+SetupPowershellProfile
+ConfigureGit
+RunAntivirus -ScanType "QuickScan"
+if ($Config.wsl_install) {
+  Install-WSL "$Config.wsl_distro"
+}
+else {
+  Show-Output ">> Skipping WSL Install (disabled by configuration option)"
+  Write-Host ""
+}
+
+
+### End
+Stop-Transcript
---
+++ provisioner-windows/utils.ps1
@@ -0,0 +1,291 @@
+### Constants
+$RepoPath = $PSScriptRoot
+New-Item -Path "$RepoPath" -Name "downloads" -ItemType "directory" -Force | Out-Null
+New-Item -Path "$RepoPath" -Name "logs" -ItemType "directory" -Force | Out-Null
+$DownloadsPath = "${RepoPath}\downloads"
+$LogsPath = "${RepoPath}\logs"
+
+### Functions
+function Show-Output() {
+  Write-Host "[provisioner] $args" -BackgroundColor White -ForegroundColor Black
+}
+
+function InitializeYAMLConfig(){
+  <#
+    .SYNOPSIS
+        Usually used once on top of the script.
+    .EXAMPLE
+        $Config = InitializeYAMLConfig
+  #>
+  Install-Module -Name powershell-yaml -Force
+  Import-Module -Name powershell-yaml
+
+  if (-Not (Test-Path ".\config.yml")) {
+    Show-Output "!!! Copying '.\config.example.yml' to (gitignored) '.\config.yml'."
+    Copy-Item ".\config.example.yml" ".\config.yml"
+  }
+
+  Show-Output "Reading .\config.yml"
+  $_content = Get-Content -Raw ".\config.yml"
+  $Config = ConvertFrom-YAML -Ordered $_content
+  return $Config
+}
+
+function New-TemporaryDirectory {
+  <#
+    .LINK
+        https://stackoverflow.com/a/34559554/13953427
+  #>
+  $parent = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  $retv = New-Item -ItemType Directory -Path (Join-Path $parent $name)
+  return $retv
+}
+
+function Test-CommandExists {
+  [OutputType([bool])]
+  Param(
+    [Parameter(Mandatory = $true)][string]$command
+  )
+  $oldPreference = $ErrorActionPreference
+  $ErrorActionPreference = "stop"
+  try {
+    if (Get-Command $command) { RETURN $true }
+  }
+  catch { return $false }
+  finally { $ErrorActionPreference = $oldPreference }
+}
+
+function Test-Admin {
+  <#
+    .SYNOPSIS
+        Test whether the script is being run as an administrator
+    .LINK
+        https://superuser.com/questions/108207/how-to-run-a-powershell-script-as-administrator
+    #>
+  [OutputType([bool])]
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  return $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function Elevate {
+  <#
+  .SYNOPSIS
+      Elevate the current process to admin privileges.
+
+      Instead of doing `#Requires -RunAsAdministrator` you can use this,
+      so that just doing ".\scriptname.ps1" works too.
+  .NOTES
+      The script that uses this function should have the following code at the top:
+      ```
+      Param(
+          [switch]$Elevated
+      )
+      ```
+      and also the following in it's header comment section:
+      ```
+      .PARAMETER Elevated
+          This parameter is for internal use to check whether an UAC prompt has already been attempted.
+      ```
+
+  .EXAMPLE
+      Elevate($MyInvocation.MyCommand.Definition)
+  .LINK
+      https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-7.2#example-5--start-powershell-as-an-administrator
+  #>
+  Param(
+    [Parameter(Mandatory = $true)][string]$command
+  )
+  if (! (Test-Admin)) {
+    if ($elevated) {
+      Show-Output "Elevation did not work."
+    }
+    else {
+      Show-Output "This script requires admin access. Elevating."
+      Show-Output "$command"
+      # Use newer PowerShell if available.
+      if (Test-CommandExists "pwsh") { $shell = "pwsh" } else { $shell = "powershell" }
+      Start-Process -FilePath "$shell" -Verb RunAs -ArgumentList ('-NoProfile -NoExit -Command "cd {0}; {1}" -elevated' -f ($pwd, $command))
+      Show-Output "The script has been started in another window. You can close this window now."
+    }
+    exit
+  }
+  Show-Output "Running as Administrator!"
+}
+
+function Test-RebootPending {
+  <#
+    .SYNOPSIS
+        Test whether the computer has a reboot pending.
+    .LINK
+        https://4sysops.com/archives/use-powershell-to-test-if-a-windows-server-is-pending-a-reboot/
+    #>
+  [OutputType([bool])]
+  $pendingRebootTests = @(
+    @{
+      Name     = "RebootPending"
+      Test     = { Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing" -Name "RebootPending" -ErrorAction Ignore }
+      TestType = "ValueExists"
+    }
+    @{
+      Name     = "RebootRequired"
+      Test     = { Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name "RebootRequired" -ErrorAction Ignore }
+      TestType = "ValueExists"
+    }
+    @{
+      Name     = "PendingFileRenameOperations"
+      Test     = { Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name "PendingFileRenameOperations" -ErrorAction Ignore }
+      TestType = "NonNullValue"
+    }
+  )
+  foreach ($test in $pendingRebootTests) {
+    $result = Invoke-Command -ScriptBlock $test.Test
+    if ($test.TestType -eq "ValueExists" -and $result) {
+      return $true
+    }
+    elseif ($test.TestType -eq "NonNullValue" -and $result -and $result.($test.Name)) {
+      return $true
+    }
+    else {
+      return $false
+    }
+  }
+}
+
+function Update-PathEnvironmentVariable() {
+  <#
+  .LINK
+      https://stackoverflow.com/a/31845512/13953427
+  #>
+  # https://stackoverflow.com/questions/17794507/reload-the-path-in-powershell#comment70758762_31845512
+  if (Test-CommandExists "refreshenv") {
+    Show-Output "Refreshing PATH Environment Variable using 'refreshenv' function from chocolatey.."
+    refreshenv
+  }
+  else {
+    Show-Output "Refreshing PATH Environment Variable.."
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+  }
+}
+
+function Install-Winget {
+  [OutputType([bool])]
+  Param()
+
+  if (Test-CommandExists "winget") {
+    Show-Output "Microsoft App Install command ('winget') exists, skipping installation."
+    return $true
+  }
+
+  if (Get-AppxPackage -Name "Microsoft.DesktopAppInstaller") {
+    Show-Output "App Installer seems to be installed on your system, but Winget was not found."
+    return $false
+  }
+
+  if (Get-AppxPackage -Name "Microsoft.WindowsStore") {
+    # https://stackoverflow.com/a/75334942/13953427
+    Show-Output "Downloading and installing latest release of winget from GitHub..."
+    $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+    $URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+    Select-Object -ExpandProperty "assets" |
+    Where-Object "browser_download_url" -Match '.msixbundle' |
+    Select-Object -ExpandProperty "browser_download_url"
+
+    # Download $URL
+    Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+    # Install
+    Add-AppxPackage -Path "Setup.msix"
+    # Cleanup
+    Remove-Item "Setup.msix"
+
+    Update-PathEnvironmentVariable
+    return (Test-CommandExists "winget");
+  }
+  Show-Output "Cannot install App Installer, as Microsoft Store appears not to be installed. This is normal on servers. Winget will not be available."
+  return $false
+}
+
+function Install-Chocolatey() {
+  <#
+  .SYNOPSIS
+      Execute command as seen in https://chocolatey.org/install#individual if "choco" command does not exist.
+  #>
+  param(
+    [switch]$Force
+  )
+  if ($Force -Or (-Not (Test-CommandExists "choco"))) {
+    Show-Output "Installing the Chocolatey package manager by downloading and running official install script"
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  }
+  # "Please use choco upgrade chocolatey to handle upgrades of Chocolatey itself."
+  Show-Output "Chocolatey command ('choco') exists. Upgrading chocolatey using chocolatey.."
+  choco upgrade chocolatey -y
+  Write-Host ""
+}
+
+function Install-1PasswordCLI() {
+  if (-Not (Test-CommandExists "op")) {
+    # From https://developer.1password.com/docs/cli/get-started/
+    Show-Output "'Installing' 1Password CLI over PowerShell (Download / Unpack / Add to PATH).."
+    $arch = (Get-CimInstance Win32_OperatingSystem).OSArchitecture
+    switch ($arch) {
+      '64-bit' { $opArch = 'amd64'; break }
+      '32-bit' { $opArch = '386'; break }
+      Default { Write-Error "Sorry, your operating system architecture '$arch' is unsupported" -ErrorAction Stop }
+    }
+    $installDir = Join-Path -Path $env:ProgramFiles -ChildPath '1Password CLI'
+    Invoke-WebRequest -Uri "https://cache.agilebits.com/dist/1P/op2/pkg/v2.4.1/op_windows_$($opArch)_v2.4.1.zip" -OutFile op.zip
+    Expand-Archive -Path op.zip -DestinationPath $installDir -Force
+    $envMachinePath = [System.Environment]::GetEnvironmentVariable('PATH', 'machine')
+    if ($envMachinePath -split ';' -notcontains $installDir) {
+      [Environment]::SetEnvironmentVariable('PATH', "$envMachinePath;$installDir", 'Machine')
+    }
+    Remove-Item -Path op.zip
+
+    # Refresh PATH to get new `op` command
+    Update-PathEnvironmentVariable
+  }
+  Show-Output "1Password CLI command ('op') exists, skipping installation."
+}
+
+function RunAntivirus() {
+  param (
+    [Parameter(Mandatory = $true)] [string] $ScanType
+  )
+
+  Show-Output ">> Run Antivirus"
+  if (Test-CommandExists "Update-MpSignature") {
+    Show-Output "Updating Windows Defender definitions. If you have another antivirus program installed, Windows Defender may be disabled, causing this to fail."
+    Update-MpSignature
+  }
+  else {
+    Show-Output "Virus definition updates are not supported - Check them manually."
+  }
+  if (Test-CommandExists "Start-MpScan") {
+    Show-Output "Running Windows Defender " $ScanType ". If you have another antivirus program installed, Windows Defender may be disabled, causing this to fail."
+    Start-MpScan -ScanType $ScanType
+  }
+  else {
+    Show-Output "Virus scan is not supported - Run it manually."
+  }
+  Write-Host ""
+}
+
+function UpdateWindows() {
+  Show-Output ">> Update Windows and Reboot if necessary."
+  Install-Module -Name PSWindowsUpdate -Scope AllUsers -Force
+  Import-Module -Name PSWindowsUpdate
+  $updates = Get-WindowsUpdate -AcceptAll -AutoReboot -Install
+  $rebootRequired = $updates.IsRebootRequired
+  if ($rebootRequired) {
+    Show-Output "Reboot required because of update! Aborting script."
+    # reboot with a warning (curiously not always initiated by above command):
+    shutdown /r
+    Stop-Transcript
+    exit 0
+  }
+  Show-Output "No Reboot required, at least not because of update!"
+  Write-Host ""
+}
